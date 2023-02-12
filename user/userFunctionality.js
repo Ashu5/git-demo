6954d2a862fcb4a30fd7a7f60d14a0f1db9b9278
Ashu5 <www.ashu5@gmail.com>
2023-02-13 01:12:22 +0530

    added the session count

--- user/userFunctionality.js
+++ user/userFunctionality.js
@@ -1,6 +1,10 @@
+const MAX_SESSION=3;
+
 var user=getCurrentUser();
  var userFuncs=getFunctionsByUser(user);
+ var session=countUserSession(user);
  console.log("USER FUNCTIONS:",userFuncs);
+ console.log("USER SESSION:",sessiong);
 
 function getCurrentUser(){
     return "CURR_USER";
@@ -9,4 +13,8 @@ function getCurrentUser(){
 function getFunctionsByUser( user){
 const userFunctions= ["LOGIN","EDIT PAGE","PLAY GAMES"]; 
 return userFunctions;
+}
+
+function countUserSession(user){
+    return MAX_SESSION;
 }
