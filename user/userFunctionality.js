d10052b3d38895c2b33502beb8c67043a2c0923b
Ashu5 <www.ashu5@gmail.com>
2023-02-16 00:44:37 +0530

    Revert "comments"
    This reverts commit 1c04ed7f67e430173ce31b50bb6d1e7615d44a70.

--- user/userFunctionality.js
+++ user/userFunctionality.js
@@ -30,8 +30,9 @@ function addUserProxy(user){
 function onBoardUser(user){
 return new User["user"];
 }
-//update user
-function updateUser(existingUser){
- user=existingUser;
+
+function updateUser(user){
+ user=getUserByName(user);
+ user="user";
  return user;
 }
